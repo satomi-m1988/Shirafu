85484e14529bc9299274d0f40ff24f88bc8090d4
vagrant <vagrant@localhost.localdomain>
2020-08-26 04:25:11 +0000

    [Make]tag model & tweet_tag model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ApplicationRecord
 	belongs_to :user
+
+	has_many :tweet_tags, dependent: :destroy
+	has_many :tags, through: :tweet_tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_061137) do
+ActiveRecord::Schema.define(version: 2020_08_26_040128) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_tags", force: :cascade do |t|
+    t.integer "tweet_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_tweet_tags_on_tag_id"
+    t.index ["tweet_id"], name: "index_tweet_tags_on_tweet_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id"
