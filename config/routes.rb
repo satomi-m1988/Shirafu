0e9bd70522066d0bfa4f6a5ac37615bc85dc3812
vagrant <vagrant@localhost.localdomain>
2020-08-31 12:35:50 +0000

    [Make] relationship controll & view

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tweets do
-    resource :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
   resources :users do
     resource :relationships, only: [:create, :destroy]
