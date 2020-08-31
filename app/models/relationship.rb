dce0c8349dcaa5d4077b0d95ff7febd32b28b8b6
vagrant <vagrant@localhost.localdomain>
2020-08-31 07:44:41 +0000

    [Modify] relationship model & user model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,4 @@
 class Relationship < ApplicationRecord
+	belongs_to :following, class_name: "User" #'class_name'はUserモデルから擬似的にfollowingモデルを作成している
+	belongs_to :follower, class_name: "User" #'class_name'はUserモデルから擬似的にfollowerモデルを作成している
 end
