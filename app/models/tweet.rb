d1779bc4aa1ec729aa26d42f8790502f06b0190d
vagrant <vagrant@localhost.localdomain>
2020-09-01 04:20:28 +0000

    [Modify] model/tweet.rb & model/tag.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,8 @@
 class Tweet < ApplicationRecord
 	belongs_to :user
 
-	has_many :tweet_tags
-	has_many :tags, through: :tweet_tags
+	has_many :tweet_tags, dependent: :destroy
+	has_many :tags, through: :tweet_tags, dependent: :destroy
 
 	after_create do
 		tags = self.body.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
