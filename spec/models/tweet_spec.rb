b6cfb3260e3969e9a1e3e27a8e85f81899761a92
vagrant <vagrant@localhost.localdomain>
2020-09-12 08:12:32 +0000

    fix RSpec models/tweet_spec.rb

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Tweet, type: :model do
 	context "データが正しく保存される" do
 		before do
+			user = User.create(name: "あああ", email: "a@a", password: "aaa111", adult: "1")
 			@tweet = Tweet.new
 			@tweet.body = "こんにちは"
 			@tweet.user_id = user.id
