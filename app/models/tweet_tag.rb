b8f52748154eb018ea384c1ba593040331d52e19
vagrant <vagrant@localhost.localdomain>
2020-09-13 06:46:33 +0000

    fix tweet_tag models 2

--- app/models/tweet_tag.rb
+++ app/models/tweet_tag.rb
@@ -1,6 +1,6 @@
 class TweetTag < ApplicationRecord
-  belongs_to :tweet, dependent: :destroy
-  belongs_to :tag, dependent: :destroy
+  belongs_to :tweet
+  belongs_to :tag
 
   validates :tweet_id, presence: true
   validates :tag_id, presence: true
